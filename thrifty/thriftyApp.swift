e97a87bab2f74bfeb6de075514fb9045f1e402f9
elliot <elliot@popina.com>
2021-06-22 21:54:52 +0200

    Initial Commit

---
+++ thrifty/thriftyApp.swift
@@ -0,0 +1,17 @@
+//
+//  thriftyApp.swift
+//  thrifty
+//
+//  Created by Elliot Cunningham on 22/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct thriftyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
